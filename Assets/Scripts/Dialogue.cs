b03631dca6b6e1f1305478ab5d181fe1d994d12f
EMYGP\EmyGP <emyburgp@gmail.com>
2020-04-20 16:17:51 +0200

    aggiunte scene, navigazione scene, MM. Iniziato dialogue system

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI MyText;
+    public string[] sentences;
+    private int index;
+    public float TextSpeed = 0.03f;
+    public GameObject nextButton;
+
+    void Start()
+    {
+        nextButton.SetActive(false);
+        StartCoroutine(Typing());
+    }
+
+    //converte ogni carattere dell'array "sentences" in char, poi aggiunge al MyText una lettera (1 char) alla volta in base alla velocità settata in textSpeed
+    IEnumerator Typing()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            MyText.text += letter;
+            yield return new WaitForSeconds(TextSpeed);
+        }
+    }
+
+    //da collegare al bottone, fa avanzare il testo andando a prendere le frasi successive dell'array ogni volta che una frase finisce
+    public void NextSentence()
+    {
+        if(index < sentences.Length)
+        {
+            index++;
+            MyText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            MyText.text = "";
+        }
+    }
+}
---
+++ Assets/Scripts/SceneOpener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//per caricare le scene. Mettere una copia di questo script per ogni scena e poi selezionare la scena dalla funz OnClick del button (wow...)
+//per chiudere il gioco e temo anche per altro in futuro
+public class SceneOpener : MonoBehaviour
+{
+    public string Scene_name;
+
+    public void GotToScene()
+    {
+         SceneManager.LoadScene(Scene_name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
